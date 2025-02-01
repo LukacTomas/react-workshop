6b0b64b1a1c43e8a1dfd3cf74e646619af96e0cd
TomasLukac <tomas.lukac@siemens-healthineers.com>
2025-02-01 13:25:38 +0100

    feat: creating useDebounce hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,18 @@ import './App.css';
 import { Heading } from './components/Heading.tsx';
 import { Footer } from './components/Footer.tsx';
 import { Search } from './components/Search.tsx';
-import { useCallback, useState } from 'react';
-
-/*
-Better structure of the code is the key to maintainability and scalability, performance, and readability.
-*/
+import { useState } from 'react';
 
 function App() {
   return (
     <>
       <Heading />
-      <BookSearch />
       <MovieSearch />
-      <CarSearch />
       <Footer />
     </>
   )
 }
 
-const BookSearch = () => {
-  const [book, setBook] = useState('');
-  const handleSearchBook = useCallback((query: string) => {
-    setBook(query);
-  }, []);
-  return (
-    <>
-      <Search placeholder="My search 1" label="Book" onSearch={handleSearchBook} />
-      <SearchResult result={book} />
-    </>
-  );
-};
 
 const MovieSearch = () => {
   const [movie, setMovie] = useState('');
@@ -45,20 +27,6 @@ const MovieSearch = () => {
     </>
   );
 };
-
-const CarSearch = () => {
-  const [car, setCar] = useState('');
-  const handleSearchCar = (query: string) => {
-    setCar(query);
-  };
-  return (
-    <>
-      <Search placeholder="My search 1" label="Car" onSearch={handleSearchCar} />
-      <SearchResult result={car} />
-    </>
-  );
-};
-
 const SearchResult = function SearchResult({ result }: { result: string }) {
   return (
     <h3> {result}</h3>
---
+++ src/components/hooks/useDebounce.ts
@@ -0,0 +1,7 @@
+/**
+ * useDebounce
+ *
+ * Create a custom hook useDebounce that debounces a given callback function.
+ * The hook should delay the execution of the callback until a specified delay period has passed since the last call.
+ *
+ */
