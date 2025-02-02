0e58099a5f7e1bb4ea1e78474653415d1f90d954
TomasLukac <tomas.lukac@siemens-healthineers.com>
2025-02-02 07:10:13 +0100

    feat: displaying fetched data - solution

--- src/components/CharacterResult.tsx
+++ src/components/CharacterResult.tsx
@@ -14,18 +14,42 @@ import { useFetch } from './hooks/useFetch.ts';
  *
  */
 
-export const CharacterResult = ({ result }: { result: string }) => {
-  const {data, loading, error} = useFetch(result);
-  console.log(data);
-  console.log(loading);
-  console.log(error);
+interface Character {
+  index:number;
+  fullName:string;
+  nickname: string;
+  hogwartsHouse:string;
+  image:string;
+  birthdate:string;
+  children: string[];
+  interpretedBy:string;
+}
 
+export const CharacterResult = ({ result }: { result: string }) => {
+  const {data, loading, error} = useFetch<Character>(result);
+
+  if (loading) {
+    return <h3>Loading...</h3>
+  }
+
+  if (error) {
+    return <h3>Error: {error}</h3>
+  }
+
+  if (!data) {
+    return <h3>No data</h3>
+  }
 
   return (
     <>
-      { /**
-       *Map trough components in here
-       */}
+      {
+        data.map( character => (
+          <div key={character.index}>
+            <p>{character.fullName}</p>
+            <p>{character.birthdate}</p>
+          </div>
+        ))
+      }
     </>
   );
 };
