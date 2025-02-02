e41b36713375854999d44dd41e244e9e582fd600
TomasLukac <tomas.lukac@siemens-healthineers.com>
2025-02-02 06:56:29 +0100

    feat: displaying fetched data

--- src/components/CharacterResult.tsx
+++ src/components/CharacterResult.tsx
@@ -1,8 +1,24 @@
 import { useFetch } from './hooks/useFetch.ts';
 
+/**
+ * Exercise
+ *
+ * Hover over "data" and you should see unknown[] | null, let's fix this
+ * Inspect the Response for searching in Network tab or in console.
+ * Create interface Character that contains the fields as in response.
+ * Pass the interface to useFetch to make the data correct type
+ * Map through  data and display at least fullName and birthdate
+ *
+ * Pro tip: don't forget to deal with case when data is null
+ * Bonus: you can use loading and error.
+ *
+ */
 
 export const CharacterResult = ({ result }: { result: string }) => {
-  useFetch(result);
+  const {data, loading, error} = useFetch(result);
+  console.log(data);
+  console.log(loading);
+  console.log(error);
 
 
   return (
--- src/components/hooks/useFetch.ts
+++ src/components/hooks/useFetch.ts
@@ -9,6 +9,60 @@
  *
  * Pro typ: make useFetch generic
  */
+import { createUrl } from '../helpers/ajax.ts';
+import { useEffect, useState } from 'react';
 
-export const useFetch = (query:string)=>{}
+export const useFetch = <T>(query: string) => {
+  const [data, setData] = useState<T[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!query) {
+      setData(null);
+      setError(null);
+      return;
+    }
+
+    async function fetchData(signal: AbortSignal) {
+      setLoading(true);
+
+      try {
+        const response = await fetch(createUrl(query));
+
+        if(!response.ok) {
+          throw new Error(response.statusText);
+        }
+        if (!signal.aborted) {
+          setData(await response.json());
+        }
+      } catch (err:unknown) {
+        if(!signal.aborted) {
+          setError((err as Error).message)
+        }
+      } finally {
+        if(!signal.aborted) {
+          setLoading(false)
+        }
+      }
+    }
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetchData(signal).catch(setError);
+
+    return () => {
+      controller.abort();
+    }
+
+  }, [query]);
+
+
+  return {
+    data,
+    loading,
+    error
+  }
+}
 
